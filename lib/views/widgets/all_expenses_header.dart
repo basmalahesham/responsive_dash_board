f2073f4a044c3f8258cb7e759c14574ace827eb7
basmalahesham <112768821+basmalahesham@users.noreply.github.com>
2024-09-18 21:56:12 +0300

    create all_expenses_header.dart widget part1

---
+++ lib/views/widgets/all_expenses_header.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class AllExpensesHeader extends StatelessWidget {
+  const AllExpensesHeader({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Row(
+      children: [
+
+      ],
+    );
+  }
+}
