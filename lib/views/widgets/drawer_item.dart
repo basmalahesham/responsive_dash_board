dbdc643988c33aea92673c98b187a39b55f33aba
basmalahesham <112768821+basmalahesham@users.noreply.github.com>
2024-09-18 20:31:47 +0300

    create active_and_inactive_item.dart

--- lib/views/widgets/drawer_item.dart
+++ lib/views/widgets/drawer_item.dart
@@ -1,20 +1,20 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_svg/flutter_svg.dart';
 import 'package:responsive_dash_board/models/drawer_item_model.dart';
-import 'package:responsive_dash_board/utils/app_styles.dart';
+import 'package:responsive_dash_board/views/widgets/active_and_inactive_item.dart';
 
 class DrawerItem extends StatelessWidget {
   const DrawerItem({
     super.key,
     required this.drawerItemModel,
+    required this.isActive,
   });
 
   final DrawerItemModel drawerItemModel;
+  final bool isActive;
   @override
   Widget build(BuildContext context) {
-    return ListTile(
-      leading: SvgPicture.asset(drawerItemModel.image),
-      title: Text(drawerItemModel.title,style: AppStyles.styleRegular16,),
-    );
+    return isActive
+        ? ActiveDrawerItem(drawerItemModel: drawerItemModel)
+        : InActiveDrawerItem(drawerItemModel: drawerItemModel);
   }
 }
--- lib/views/widgets/drawer_item_list_view.dart
+++ lib/views/widgets/drawer_item_list_view.dart
@@ -3,14 +3,24 @@ import 'package:responsive_dash_board/models/drawer_item_model.dart';
 import 'package:responsive_dash_board/utils/generated/assets.dart';
 import 'package:responsive_dash_board/views/widgets/drawer_item.dart';
 
-class DrawerItemListView extends StatelessWidget {
+class DrawerItemListView extends StatefulWidget {
   const DrawerItemListView({super.key});
-  static const List<DrawerItemModel> items = [
-    DrawerItemModel(title: 'Dashboard', image: Assets.imagesDashboard),
-    DrawerItemModel(title: 'My Transaction', image: Assets.imagesMyTransctions),
-    DrawerItemModel(title: 'Statistics', image: Assets.imagesStatistics),
-    DrawerItemModel(title: 'Wallet Account', image: Assets.imagesWalletAccount),
-    DrawerItemModel(title: 'My Investments', image: Assets.imagesMyInvestments),
+
+  @override
+  State<DrawerItemListView> createState() => _DrawerItemListViewState();
+}
+
+class _DrawerItemListViewState extends State<DrawerItemListView> {
+  int activeIndex = 0;
+  final List<DrawerItemModel> items = [
+    const DrawerItemModel(title: 'Dashboard', image: Assets.imagesDashboard),
+    const DrawerItemModel(
+        title: 'My Transaction', image: Assets.imagesMyTransctions),
+    const DrawerItemModel(title: 'Statistics', image: Assets.imagesStatistics),
+    const DrawerItemModel(
+        title: 'Wallet Account', image: Assets.imagesWalletAccount),
+    const DrawerItemModel(
+        title: 'My Investments', image: Assets.imagesMyInvestments),
   ];
 
   @override
@@ -20,10 +30,20 @@ class DrawerItemListView extends StatelessWidget {
       physics: const NeverScrollableScrollPhysics(),
       itemCount: items.length,
       itemBuilder: (context, index) {
-        return Padding(
-          padding: const EdgeInsets.only(top: 20.0),
-          child: DrawerItem(
-            drawerItemModel: items[index],
+        return GestureDetector(
+          onTap: () {
+            if (activeIndex != index) {
+              setState(() {
+                activeIndex = index;
+              });
+            }
+          },
+          child: Padding(
+            padding: const EdgeInsets.only(top: 20.0),
+            child: DrawerItem(
+              drawerItemModel: items[index],
+              isActive: activeIndex == index,
+            ),
           ),
         );
       },
