aa57b28a395c524154e93bc6851e73b327dfc167
basmalahesham <112768821+basmalahesham@users.noreply.github.com>
2024-09-19 02:54:32 +0300

    create all_expenses_items_list_view.dart part1

---
+++ lib/views/widgets/all_expenses_items_list_view.dart
@@ -0,0 +1,39 @@
+import 'package:flutter/material.dart';
+import 'package:responsive_dash_board/models/all_expenses_item_model.dart';
+import 'package:responsive_dash_board/utils/generated/assets.dart';
+import 'package:responsive_dash_board/views/widgets/all_expenses_item.dart';
+
+class AllExpensesItemsListView extends StatelessWidget {
+  const AllExpensesItemsListView({super.key});
+  static const List<AllExpensesItemModel> items = [
+    AllExpensesItemModel(
+      image: Assets.imagesBalance,
+      title: 'Balance',
+      date: 'April 2022',
+      price: r'$20,129',
+    ),
+    AllExpensesItemModel(
+      image: Assets.imagesIncome,
+      title: 'Income',
+      date: 'April 2022',
+      price: r'$20,129',
+    ),
+    AllExpensesItemModel(
+      image: Assets.imagesExpenses,
+      title: 'Expenses',
+      date: 'April 2022',
+      price: r'$20,129',
+    ),
+  ];
+  @override
+  Widget build(BuildContext context) {
+    return ListView.builder(
+      itemCount: items.length,
+      itemBuilder: (context, index) {
+        return AllExpensesItem(
+          allExpensesItemModel: items[index],
+        );
+      },
+    );
+  }
+}
