a0927c3f8833ad082ee2b2da2b6c715261d8221c
basmalahesham <112768821+basmalahesham@users.noreply.github.com>
2024-09-19 05:26:41 +0300

    using AnimatedCrossFade

--- lib/views/widgets/all_expenses_item.dart
+++ lib/views/widgets/all_expenses_item.dart
@@ -3,17 +3,30 @@ import 'package:responsive_dash_board/models/all_expenses_item_model.dart';
 import 'package:responsive_dash_board/views/widgets/inactive_and_active_all_expenses_item.dart';
 
 class AllExpensesItem extends StatelessWidget {
-  const AllExpensesItem(
-      {super.key,
-      required this.allExpensesItemModel,
-      required this.isSelected});
+  const AllExpensesItem({
+    super.key,
+    required this.allExpensesItemModel,
+    required this.isSelected,
+  });
 
   final AllExpensesItemModel allExpensesItemModel;
   final bool isSelected;
+
   @override
   Widget build(BuildContext context) {
-    return isSelected
-        ? ActiveAllExpensesItem(allExpensesItemModel: allExpensesItemModel)
-        : InActiveAllExpensesItem(allExpensesItemModel: allExpensesItemModel);
+    return AnimatedCrossFade(
+      firstChild: InActiveAllExpensesItem(allExpensesItemModel: allExpensesItemModel),
+      secondChild: ActiveAllExpensesItem(allExpensesItemModel: allExpensesItemModel),
+      crossFadeState: isSelected ? CrossFadeState.showSecond : CrossFadeState.showFirst,
+      duration: const Duration(milliseconds: 300), // Adjust duration as needed
+      layoutBuilder: (topChild, topKey, bottomChild, bottomKey) {
+        return Stack(
+          children: <Widget>[
+            Positioned(key: bottomKey, child: bottomChild),
+            Positioned(key: topKey, child: topChild),
+          ],
+        );
+      },
+    );
   }
 }
