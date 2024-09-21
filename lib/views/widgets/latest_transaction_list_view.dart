d1eea91df302fca58c77afcf4db3703bd11c5c6d
basmalahesham <112768821+basmalahesham@users.noreply.github.com>
2024-09-22 01:54:10 +0300

    define height for the latest_transaction_list_view.dart & use IntrinsicWidth with UserInfoListTile

--- lib/views/widgets/latest_transaction_list_view.dart
+++ lib/views/widgets/latest_transaction_list_view.dart
@@ -1,10 +1,43 @@
 import 'package:flutter/material.dart';
+import 'package:responsive_dash_board/models/user_info_model.dart';
+import 'package:responsive_dash_board/utils/generated/assets.dart';
+import 'package:responsive_dash_board/views/widgets/user_info_list_tile.dart';
 
 class LatestTransactionListView extends StatelessWidget {
   const LatestTransactionListView({super.key});
 
+  static const items = [
+    UserInfoModel(
+      image: Assets.imagesAvatar1,
+      title: 'Madrani Andi',
+      subTitle: 'Madraniadi20@gmail',
+    ),
+    UserInfoModel(
+      image: Assets.imagesAvatar2,
+      title: 'Josua Nunito',
+      subTitle: 'Josh Nunito@gmail.com',
+    ),
+    UserInfoModel(
+      image: Assets.imagesAvatar3,
+      title: 'Madrani Andi',
+      subTitle: 'Madraniadi20@gmail',
+    ),
+  ];
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return SizedBox(
+      height: 80,
+      child: ListView.builder(
+        scrollDirection: Axis.horizontal,
+        itemCount: items.length,
+        itemBuilder: (context, index) {
+          return IntrinsicWidth(
+            child: UserInfoListTile(
+              userInfoModel: items[index],
+            ),
+          );
+        },
+      ),
+    );
   }
 }
--- lib/views/widgets/quick_invoice.dart
+++ lib/views/widgets/quick_invoice.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:responsive_dash_board/views/widgets/custom_background_container.dart';
+import 'package:responsive_dash_board/views/widgets/latest_transaction.dart';
 import 'package:responsive_dash_board/views/widgets/quick_invoice_header.dart';
 
 class QuickInvoice extends StatelessWidget {
@@ -11,6 +12,7 @@ class QuickInvoice extends StatelessWidget {
       child: Column(
         children: [
           QuickInvoiceHeader(),
+          LatestTransaction(),
         ],
       ),
     );
