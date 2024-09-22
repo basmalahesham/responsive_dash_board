6d4708810a226c3bba2525b1013e7a223c4051f4
basmalahesham <112768821+basmalahesham@users.noreply.github.com>
2024-09-22 04:49:13 +0300

    create quick_invoice_form.dart

--- lib/views/widgets/quick_invoice.dart
+++ lib/views/widgets/quick_invoice.dart
@@ -1,8 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:responsive_dash_board/views/widgets/custom_background_container.dart';
 import 'package:responsive_dash_board/views/widgets/latest_transaction.dart';
+import 'package:responsive_dash_board/views/widgets/quick_invoice_form.dart';
 import 'package:responsive_dash_board/views/widgets/quick_invoice_header.dart';
-import 'package:responsive_dash_board/views/widgets/title_text_field.dart';
 
 class QuickInvoice extends StatelessWidget {
   const QuickInvoice({super.key});
@@ -19,8 +19,10 @@ class QuickInvoice extends StatelessWidget {
           LatestTransaction(),
           Divider(
             height: 48,
+            color: Color(0xffF1F1F1),
           ),
-          TitleTextField(title: 'Customer name', hint: 'Type customer name'),        ],
+          QuickInvoiceForm(),
+        ],
       ),
     );
   }
