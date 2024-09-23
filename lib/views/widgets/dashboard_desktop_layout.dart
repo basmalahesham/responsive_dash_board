4fe2d93a68b51755439b8820df099cb24827cd99
basmalahesham <112768821+basmalahesham@users.noreply.github.com>
2024-09-23 21:44:17 +0300

    create income_section.dart & income_section_header.dart

--- lib/views/widgets/dashboard_desktop_layout.dart
+++ lib/views/widgets/dashboard_desktop_layout.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:responsive_dash_board/views/widgets/all_expenses_and_quick_invoice_section.dart';
 import 'package:responsive_dash_board/views/widgets/custom_drawer.dart';
-import 'package:responsive_dash_board/views/widgets/my_card_and_transaction_history_section.dart';
+import 'package:responsive_dash_board/views/widgets/income_section.dart';
 
 class DashboardDesktopLayout extends StatelessWidget {
   const DashboardDesktopLayout({super.key});
@@ -21,7 +21,7 @@ class DashboardDesktopLayout extends StatelessWidget {
         SizedBox(
           width: 24,
         ),
-        Expanded(child: MyCardAndTransactionHistorySection()),
+        Expanded(child: IncomeSection()),
       ],
     );
   }
