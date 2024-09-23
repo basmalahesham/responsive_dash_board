cb177276e7bd8a0c5efcd4e3c3df7e88d77e3128
basmalahesham <112768821+basmalahesham@users.noreply.github.com>
2024-09-23 19:20:19 +0300

    create my_cards_section.dart

--- lib/views/widgets/dashboard_desktop_layout.dart
+++ lib/views/widgets/dashboard_desktop_layout.dart
@@ -1,8 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:responsive_dash_board/views/widgets/all_expenses_and_quick_invoice_section.dart';
 import 'package:responsive_dash_board/views/widgets/custom_drawer.dart';
-import 'package:responsive_dash_board/views/widgets/dots_indicator.dart';
-import 'package:responsive_dash_board/views/widgets/my_cards_page_view.dart';
+import 'package:responsive_dash_board/views/widgets/my_cards_section.dart';
 
 class DashboardDesktopLayout extends StatelessWidget {
   const DashboardDesktopLayout({super.key});
@@ -22,10 +21,7 @@ class DashboardDesktopLayout extends StatelessWidget {
         SizedBox(
           width: 24,
         ),
-        Expanded(
-          child: MyCardsPageView(),
-        ),
-        DotsIndicator(),
+        Expanded(child: MyCardsSection()),
       ],
     );
   }
