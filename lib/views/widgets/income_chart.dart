c54f6acb86d27018e23637fce8becc146322ec9c
basmalahesham <112768821+basmalahesham@users.noreply.github.com>
2024-09-23 22:08:02 +0300

    create income_chart.dart

---
+++ lib/views/widgets/income_chart.dart
@@ -0,0 +1,57 @@
+import 'package:fl_chart/fl_chart.dart';
+import 'package:flutter/material.dart';
+
+class IncomeChart extends StatefulWidget {
+  const IncomeChart({super.key});
+
+  @override
+  State<IncomeChart> createState() => _IncomeChartState();
+}
+
+class _IncomeChartState extends State<IncomeChart> {
+  int activeIndex = -1;
+  @override
+  Widget build(BuildContext context) {
+    return PieChart(getChartData());
+  }
+
+  PieChartData getChartData() {
+    return PieChartData(
+      sectionsSpace: 0,
+      pieTouchData: PieTouchData(
+        enabled: true,
+        touchCallback: (p0, pieTouchResponse) {
+          activeIndex =
+              pieTouchResponse?.touchedSection?.touchedSectionIndex ?? -1;
+          setState(() {});
+        },
+      ),
+      sections: [
+        PieChartSectionData(
+          showTitle: false,
+          radius: activeIndex == 0 ? 60 : 50,
+          value: 40,
+          color: const Color(0xFF208BC7),
+        ),
+        PieChartSectionData(
+          showTitle: false,
+          radius: activeIndex == 1 ? 60 : 50,
+          value: 25,
+          color: const Color(0xFF4DB7F2),
+        ),
+        PieChartSectionData(
+          showTitle: false,
+          radius: activeIndex == 2 ? 60 : 50,
+          value: 20,
+          color: const Color(0xFF064060),
+        ),
+        PieChartSectionData(
+          showTitle: false,
+          radius: activeIndex == 3 ? 60 : 50,
+          value: 22,
+          color: const Color(0xFFE2DECD),
+        ),
+      ],
+    );
+  }
+}
--- lib/views/widgets/income_section.dart
+++ lib/views/widgets/income_section.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:responsive_dash_board/views/widgets/custom_background_container.dart';
+import 'package:responsive_dash_board/views/widgets/income_chart.dart';
 import 'package:responsive_dash_board/views/widgets/income_section_header.dart';
 
 class IncomeSection extends StatelessWidget {
@@ -10,7 +11,14 @@ class IncomeSection extends StatelessWidget {
     return const CustomBackgroundContainer(
       child: Column(
         children: [
-          IncomeSectionHeader()
+          IncomeSectionHeader(),
+          Expanded(
+            child: Row(
+              children: [
+                Expanded(child: IncomeChart()),
+              ],
+            ),
+          ),
         ],
       ),
     );
