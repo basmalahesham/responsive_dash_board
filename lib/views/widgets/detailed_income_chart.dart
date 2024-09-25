7224b13e4003578a14342a29afe75c0e9506e1ae
basmalahesham <112768821+basmalahesham@users.noreply.github.com>
2024-09-25 22:40:48 +0300

    detailed_income_chart.dart part two

--- lib/views/widgets/detailed_income_chart.dart
+++ lib/views/widgets/detailed_income_chart.dart
@@ -1,5 +1,6 @@
 import 'package:fl_chart/fl_chart.dart';
 import 'package:flutter/material.dart';
+import 'package:responsive_dash_board/utils/app_styles.dart';
 
 class DetailedIncomeChart extends StatefulWidget {
   const DetailedIncomeChart({super.key});
@@ -33,27 +34,39 @@ class _IncomeChartState extends State<DetailedIncomeChart> {
       ),
       sections: [
         PieChartSectionData(
-          showTitle: false,
           radius: activeIndex == 0 ? 60 : 50,
+          title: activeIndex == 0 ? 'Design service' : '40%',
+          titlePositionPercentageOffset: activeIndex == 0 ? 1.5 : null,
           value: 40,
+          titleStyle: AppStyles.styleMedium16
+              .copyWith(color: activeIndex == 0 ? null : Colors.white),
           color: const Color(0xFF208BC7),
         ),
         PieChartSectionData(
-          showTitle: false,
           radius: activeIndex == 1 ? 60 : 50,
+          title: activeIndex == 1 ? 'Design product' : '25%',
+          titlePositionPercentageOffset: activeIndex == 1 ? 2.2 : null,
           value: 25,
+          titleStyle: AppStyles.styleMedium16
+              .copyWith(color: activeIndex == 1 ? null : Colors.white),
           color: const Color(0xFF4DB7F2),
         ),
         PieChartSectionData(
-          showTitle: false,
           radius: activeIndex == 2 ? 60 : 50,
+          title: activeIndex == 2 ? 'Product royalti' : '20%',
+          titlePositionPercentageOffset: activeIndex == 2 ? 1.4 : null,
           value: 20,
+          titleStyle: AppStyles.styleMedium16
+              .copyWith(color: activeIndex == 2 ? null : Colors.white),
           color: const Color(0xFF064060),
         ),
         PieChartSectionData(
-          showTitle: false,
           radius: activeIndex == 3 ? 60 : 50,
+          title: activeIndex == 3 ? 'Other' : '22%',
+          titlePositionPercentageOffset: activeIndex == 3 ? 1.5 : null,
           value: 22,
+          titleStyle: AppStyles.styleMedium16
+              .copyWith(color: activeIndex == 3 ? null : Colors.white),
           color: const Color(0xFFE2DECD),
         ),
       ],
